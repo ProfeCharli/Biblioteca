334261419a8fef490971c568d2e8ae0d7aa13e41
Profe Charli <carlossanchez@uas.edu.mx>
2025-02-25 11:21:09 -0700

    Clase Cargo

---
+++ src/biblioteca/Cargo.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package biblioteca;
+
+/**
+ *
+ * @author Charli
+ */
+public class Cargo {
+    private int id_cargo;
+    private String nombre;
+
+    public Cargo() {
+    }
+
+    public Cargo(int id_cargo, String nombre) {
+        this.id_cargo = id_cargo;
+        this.nombre = nombre;
+    }
+
+    public int getId_cargo() {
+        return id_cargo;
+    }
+
+    public void setId_cargo(int id_cargo) {
+        this.id_cargo = id_cargo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+}
