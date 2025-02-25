54a703403ff8a54a5d4e59f746c67a4a6757e731
Profe Charli <carlossanchez@uas.edu.mx>
2025-02-25 11:40:52 -0700

    Clase Editorial

--- src/biblioteca/Editorial.java
+++ src/biblioteca/Editorial.java
@@ -10,5 +10,42 @@ package biblioteca;
  * @author Charli
  */
 public class Editorial {
+    private int id_editorial;
+    private String nombre;
+    private String direccion;
+    
+    public Editorial() {
+    }
+
+    public Editorial(int id_editorial, String nombre, String direccion) {
+        this.id_editorial = id_editorial;
+        this.nombre = nombre;
+        this.direccion = direccion;
+    }
+
+    public int getId_editorial() {
+        return id_editorial;
+    }
+
+    public void setId_editorial(int id_editorial) {
+        this.id_editorial = id_editorial;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    
     
 }
